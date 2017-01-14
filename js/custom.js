b7dffa4854bd351912c80bee61f1ecc216249276
Anuj Sharma <anujshama4u@gmail.com>
2017-01-14 14:17:35 +0530

    added countdown using flipclock

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
 $(function(){
+	//For navbar on mobile devices
 	$(".button-collapse").sideNav();
+
+	//For Countdown
+	var startDate = new Date();
+	// Do your operations
+	var endDate   = new Date('February 17, 2017');
+	var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+
+	var clock = $('.clock').FlipClock(seconds, {
+			clockFace: 'DailyCounter',
+			countdown: true
+		});
 });
