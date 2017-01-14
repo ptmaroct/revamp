7b5adcd93f7b623122a16d7b4000f321db298b94
Anuj Sharma <anujshama4u@gmail.com>
2017-01-14 21:40:44 +0530

    fixed navbar and added a sample modal

--- js/custom.js
+++ js/custom.js
@@ -4,18 +4,11 @@ $(function(){
 	$(window).load(function() {
 	$(".progress").fadeOut("slow");
 
-	//For Countdown
-	var startDate = new Date();
-	// Do your operations
-	var endDate   = new Date('February 17, 2017');
-	var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
-
+	//Countdown Timer
 	$('#myCounter').mbComingsoon({
 	expiryDate: new Date('February 17, 2017'),
 	interval: 1000,       // Counter uopdate interval
 	speed: 500         
-	}); 
-
-	
+	}); 	
 })
 });
