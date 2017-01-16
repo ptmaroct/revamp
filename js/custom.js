2d5d307585b4061ede4c402f56e5cff36a179db5
Anuj Sharma <anujshama4u@gmail.com>
2017-01-16 23:36:00 +0530

    reg.html

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,7 @@ $(function(){
 	$(window).load(function() {
 	$(".progress").fadeOut("slow");
 
-	//Countdown Timer
-	$('#myCounter').mbComingsoon({
-	expiryDate: new Date('February 17, 2017'),
-	interval: 1000,       // Counter uopdate interval
-	speed: 500         
-	}); 
+	
 
 	$('.card').hover(
 	    function() {
